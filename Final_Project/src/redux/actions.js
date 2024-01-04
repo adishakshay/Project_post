54731043380331f8e08b11ecc46ce30895eef0fe
adishakshay <118244753+adishakshay@users.noreply.github.com>
2024-01-04 12:29:07 +0530

    Delete Final_Project directory

--- Final_Project/src/redux/actions.js
+++
@@ -1,9 +0,0 @@
-const incrementAsync=()=>{
-    return(dispatch)=>{
-        setTimeout(()=>{
-            dispatch({type:'INCREMENT'});
-        },6000);
-    };
-};
-
-export default incrementAsync;
--- Final_Project/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./reducers";
-
-const store=configureStore({
-    reducer:counterReducer
-});
-
-export default store;
